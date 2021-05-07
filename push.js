f01199c21dd7cc6950439abddc46137aa266edf5
haythemBenMechichi <haythem.benmechichi@esprit.tn>
2021-05-07 09:57:12 +0100

    first comm

--- push.js
+++ push.js
@@ -22,7 +22,7 @@ const privateVapidKey=
 webpush.setVapidDetails('mailto:test@test.com', publicVapidKey,privateVapidKey);
 
     const payload= JSON.stringify({ title : 'Push Test' });
-    let=sub={};
+    let sub={"endpoint":"https://fcm.googleapis.com/fcm/send/dZ7srcbYvS0:APA91bGcQN1Hm_85Zg9xn0-m4VsAhAjUYzhivtUx52rxxZXc66XrRwcSNdV_OQXyPFC5MMMZclhzROK_DJFsgQINZYZZjSwNUNvsquc9A7UHZ8NZPZqYBsP20TBMhmN4lOgihbF1ishY","expirationTime":null,"keys":{"p256dh":"BN1hvFNC7L7lN6xmNBzAHn_KAKv4zbGgo2XMpSGEbQR6feA-JJOveioCYGYpxK3_6cmDGcdTmWc_2N5qfGFVOo8","auth":"NDQm24N9qx1BuI7Np5zz2w"}};
     webpush.sendNotification(sub,"message").catch(err => console.error(err))
 
 
